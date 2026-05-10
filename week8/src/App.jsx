09031c6a2a9caf644fc7bd6bc15cbfbd1baf55e2
jwlee <jwon04lee@gmail.com>
2026-05-10 21:00:46 +0900

    chore: 파일 정리, 폰트 적용

---
+++ week8/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Header from './components/Header.jsx'
+import Body from './components/Body.jsx'
+import Footer from './components/Footer.jsx'
+import styled from 'styled-components'
+
+const App = () => {
+  return (
+    <Container>
+      <CardBox>
+        <Header />
+        <Body />
+        <Footer />
+      </CardBox>
+    </Container>
+  )
+}
+
+export default App
+
+const Container = styled.div`
+  display: flex; 
+  justify-content: center; 
+  align-items: center; 
+  min-height: 100vh; 
+  background: #f9fafb;
+`
+const CardBox = styled.div`
+  background: white; 
+  border-radius: 20px; 
+  padding: 40px;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.08);
+`
