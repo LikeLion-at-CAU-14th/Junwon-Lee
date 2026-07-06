ee86d0aa339aad9ed9a560f99c00380b97adc03a
jwlee <jwon04lee@gmail.com>
2026-07-06 22:23:44 +0900

    feat: UserInfoContext 설계, Provider 연결

--- week13/src/App.jsx
+++ week13/src/App.jsx
@@ -3,15 +3,37 @@ import Layout from './components/layout/Layout'
 import Home from './pages/Home'
 import { Route, Routes } from 'react-router-dom'
 import MyPage from './pages/MyPage'
+import { useReducer } from 'react'
+import { UserInfoContext } from './context/UserInfoContext'
+
+const initialState = {
+  name: '',
+  email: '',
+  birth: '',
+  gender: '',
+};
+
+const userInfoReducer = (state, action) => {
+  switch(action.type) {
+    case 'SET_USER_INFO':
+      return { ...state, ...action.payload };
+    default:
+      return state;
+  }
+};
 
 const App = () => {
+  const [state, dispatch] = useReducer(userInfoReducer, initialState);
+
   return (
-    <Layout>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/mypage' element={<MyPage />} />
-      </Routes>
-    </Layout>
+    <UserInfoContext.Provider value={{ state, dispatch }}>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/mypage' element={<MyPage />} />
+        </Routes>
+      </Layout>
+    </UserInfoContext.Provider>
   )
 }
 
